7643f981e0cea39d8c57cb0d2b88c0eecf178d82
Truan dep troai <trantnce180829@fpt.edu.vn>
2025-09-30 09:31:40 +0700

    mới image sukuna

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <p>Project deploy từ git sang vercel để build</p>
       <footer className="justify-center">
-        <Image src="/nezuko.png" alt="lỗi ảnh" width={800} height={800} />
+        <Image src="/sukuna.png" alt="lỗi ảnh" width={800} height={800} />
       </footer>
     </div>
   );
