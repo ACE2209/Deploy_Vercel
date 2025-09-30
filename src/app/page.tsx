751abbf5b428666e2223962acd52ac8449e73e51
Truan dep troai <trantnce180829@fpt.edu.vn>
2025-09-30 09:29:14 +0700

    mới image 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,8 @@ export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <p>Project deploy từ git sang vercel để build</p>
-      <footer className="flex gap-[24px] flex-wrap items-center justify-center">
-        <Image
-          src="/nezuko.png"
-          alt="Globe icon"
-          width={800}
-          height={800}
-        />
+      <footer className="justify-center">
+        <Image src="/nezuko.png" alt="lỗi ảnh" width={800} height={800} />
       </footer>
     </div>
   );
